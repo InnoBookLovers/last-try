4ddfeae3b266d28a9de21aae9d1d23442c57b1c8
nikolaevaElizaveta <118183018+nikolaevaElizaveta@users.noreply.github.com>
2023-07-25 11:22:46 +0300

    Delete src directory

--- src/lib/Error.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let error: Error | undefined;
-</script>
-
-{#if error}
-	<strong class="text-red-600">{error.message}</strong>
-{/if}
--- src/lib/Store.ts
+++
@@ -1,5 +0,0 @@
-import type { User } from '@supabase/supabase-js';
-import { writable } from 'svelte/store';
-
-// Create a writable session store to hold the user data
-export const userStore = writable<User | null>(null);
